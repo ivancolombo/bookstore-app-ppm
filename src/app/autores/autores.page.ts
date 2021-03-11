d14052d39a1a7f805018b8b0a9b4ac5fa1aa6d68
Ivan Colombo <ivancolombo98.ic@gmail.com>
2021-03-10 22:01:15 -0300

    Iniciado a listagem de autores

--- src/app/autores/autores.page.ts
+++ src/app/autores/autores.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Autor } from './autor.model';
+import { Genero } from './genero.enum';
 
 @Component({
   selector: 'app-autores',
@@ -7,7 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutoresPage implements OnInit {
 
-  constructor() { }
+  autores: Autor[];
+
+  constructor() { 
+    this.autores = [
+      {
+        nome: 'Ivan',
+        dataNascimento: new Date(1998, 1, 13),
+        genero: Genero.MASCULINO
+      },
+      {
+        nome: 'Felipe',
+        dataNascimento: new Date(1981, 4, 19),
+        genero: Genero.FEMININO
+      },
+      {
+        nome: 'Leonardo',
+        dataNascimento: new Date(1811, 9, 29),
+        genero: Genero.FEMININO
+      },
+    ]
+  }
 
   ngOnInit() {
   }
